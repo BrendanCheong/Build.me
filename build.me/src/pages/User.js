787c3c89008cecfc87e328d882d7946ae8544db2
Brendan Cheong <brendancej82@gmail.com>
2021-05-31 21:23:52 +0800

    fixed User page bug so now no crashing

--- build.me/src/pages/User.js
+++ build.me/src/pages/User.js
@@ -1,33 +1,14 @@
-import Modal from "../components/Modal";
-import {useState,} from "react";
 import {useSpring, animated} from 'react-spring';
-import axiosInstance from "../AxiosInstance";
-
-
-
 
 const User = () => {
 
-    const [isOpenModal, setIsOpenModal] = useState(false)
-
-    const animation = useSpring({
-        config: {
-            duration: 250,
-        },
-        opacity: isOpenModal ? 1 : 0,
-        transform: isOpenModal ? `translateY(0%)` : `translateY(-100%)`,
-    })
-
-    const AmazonScrapper = async (input) => {
-        try {
-            // process input to remove spaces
-            const Input = input.replace(" ", "%20")
-            const response = await axiosInstance.get(`/Ascrapper/${Input}`)
-            return response.data
-        } catch(err) {
-            return err
-        }
-    };
+    // const animation = useSpring({
+    //     config: {
+    //         duration: 250,
+    //     },
+    //     opacity: isOpenModal ? 1 : 0,
+    //     transform: isOpenModal ? `translateY(0%)` : `translateY(-100%)`,
+    // })
 
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
@@ -37,16 +18,6 @@ const User = () => {
             <p className="text-xl text-black font-poppins">
             editable user settings
             </p>
-
-            {/** Modal Starts Here */}
-                <button onClick={() => setIsOpenModal(true)}> 
-                    Open Modal
-                </button>
-                <Modal open={isOpenModal} onClose={() =>setIsOpenModal(false)} animated={animated} animation={animation} >
-                    Fancy Text
-                </Modal>
-            {/** Modal Ends here */}
-            <button onClick={async () =>console.log( await AmazonScrapper("Ryzen 5 3600X"))}> Test Button</button>
         </div>
     )
 }
