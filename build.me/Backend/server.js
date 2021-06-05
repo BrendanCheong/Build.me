e4b704281fd97c5db58c7db2e60c34694a86b02f
Brendan Cheong <brendancej82@gmail.com>
2021-06-05 22:19:20 +0800

    Created Login Authentication and Middleware with Encryption

--- build.me/Backend/server.js
+++ build.me/Backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 // import router files
 const CPURouter = require('./routes/CPUs');
 const UserRouter = require('./routes/Users');
 const CardRouter = require('./routes/Cards');
+const BuilderRouter = require('./routes/Builder');
 const AmazonScrapper = require('./scrapper/amazonScrapper');
 
 
@@ -20,13 +22,17 @@ app.get('/', (req, res) => {
 })
 
 //BodyParser Middleware
-app.use(cors());
+app.use(cors({origin: true, credentials: true}));
 app.use(express.json());
+app.use(cookieParser());
 
 // Mongoose Middleware
 const uri = process.env.ATLAS_URI; // uses the .env file for password and link to MongdDB
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true,  useFindAndModify: false, }
-);
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true,  useFindAndModify: false, },
+    (err) => {
+        if (err) return console.error(err)
+    });
+
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully!");
@@ -36,6 +42,7 @@ connection.once('open', () => {
 app.use('/CPUs', CPURouter); 
 app.use('/users', UserRouter);
 app.use('/Cards', CardRouter);
+app.use('/Builder',BuilderRouter)
 
 // these are for ALL the SCRAPPERS
 app.get('/Ascrapper/:id', async (req, res) => { // scraper just for AMAZON
--- build.me/src/AxiosInstance.js
+++ build.me/src/AxiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({ // this defines the GLOBAL URL endpoint, to be changed to heroku URL when we migrate the Heroku
-    baseURL: "http://localhost:5000"
+    baseURL: "http://localhost:5000",
+    withCredentials: true,
 })
 
 export default axiosInstance;
