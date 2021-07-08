ca3654877ee2c9d364ff73b35a0a6a46b085e8ee
joszx <joshua_rockz@hotmail.com>
2021-07-08 14:13:17 +0800

    Added qoo10 scrapper

---
+++ build.me/Backend/scrapper/qoo10Scrapper.js
@@ -0,0 +1,99 @@
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+
+async function scrapeQoo10(itemName) {
+  return await scrapeQoo10Items(itemName);
+}
+
+async function scrapeQoo10Items(itemSearch) {
+  const URL = "https://www.qoo10.sg";
+
+  puppeteer.use(StealthPlugin());
+
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--single-process",
+      "--disable-dev-shm-usage",
+      "--no-zygote",
+    ],
+  });
+
+  try {
+    const page = await browser.newPage();
+    await page.goto(URL, { waitUntil: "networkidle0" });
+    // Searching for item on qoo10
+    await page.type("#search____keyword", itemSearch);
+    await page.click("#search_____form > fieldset > button");
+    await page.waitForNavigation({ waitUntil: "networkidle0" });
+    await scroll(page);
+
+    const scriptHTML = await page.evaluate(() => {
+      function getStringBtwn(itemRating, str1, str2) {
+        return itemRating.substring(
+          itemRating.lastIndexOf(str1) + 1,
+          itemRating.lastIndexOf(str2)
+        );
+      }
+      return Array.from(
+        document.querySelectorAll(
+          "#div_search_result_list > table > tbody > tr"
+        ),
+        (element) => {
+          let itemRating = element.querySelector(
+            "td:nth-child(5) > span.rate_v > span"
+          ).innerText;
+          itemRating =
+            itemRating === "New" ? "NA" : getStringBtwn(itemRating, "(", "/");
+
+          return {
+            itemName: element.querySelector("div > div.sbj > a").innerText,
+            itemRating: itemRating,
+            itemPrice: element.querySelector("td.td_prc > div.prc > strong")
+              .innerText,
+            itemVendor: element
+              .querySelector("td.td_item > div > div.opt_dtl > a.lnk_sh")
+              .getAttribute("title"),
+            itemURL: element
+              .querySelector("td.td_item > div.inner > div.sbj > a")
+              .getAttribute("href"),
+            itemImg: element
+              .querySelector("td.td_thmb > div.inner > a > img")
+              .getAttribute("src"),
+          };
+        }
+      );
+    });
+
+    return scriptHTML;
+  } catch (error) {
+    throw error;
+  } finally {
+    await browser.close();
+  }
+}
+
+// Scroll downwards slowly
+async function scroll(page) {
+  await page.evaluate(async () => {
+    await new Promise((resolve) => {
+      // Adjust as necessary
+      const y = 50,
+        speed = 20;
+      let heightScrolled = 0;
+
+      setInterval(() => {
+        window.scrollBy(0, y);
+        heightScrolled += y;
+        if (heightScrolled >= document.body.scrollHeight) {
+          resolve();
+        }
+      }, speed);
+    });
+  });
+}
+
+module.exports.qoo10Scraper = scrapeQoo10;
--- build.me/Backend/server.js
+++ build.me/Backend/server.js
@@ -1,128 +1,133 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const exphbs = require('express-handlebars');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const exphbs = require("express-handlebars");
+const path = require("path");
 // import router files
-const CPURouter = require('./routes/CPUs');
-const GPURouter = require('./routes/GPU');
-const UserRouter = require('./routes/Users');
-const CardRouter = require('./routes/Cards');
-const RAMRouter = require('./routes/RAM');
-const MoboRouter = require('./routes/Mobo');
-const PSURouter = require('./routes/PSU');
-const StorageRouter = require('./routes/Storage');
-const BuilderRouter = require('./routes/Builder');
+const CPURouter = require("./routes/CPUs");
+const GPURouter = require("./routes/GPU");
+const UserRouter = require("./routes/Users");
+const CardRouter = require("./routes/Cards");
+const RAMRouter = require("./routes/RAM");
+const MoboRouter = require("./routes/Mobo");
+const PSURouter = require("./routes/PSU");
+const StorageRouter = require("./routes/Storage");
+const BuilderRouter = require("./routes/Builder");
 // const AmazonRouter = require('./routes/Scraper/amazonScrapper');
 // const LazadaRouter = require('./routes/Scraper/LazadaScrapper')
 
-const AmazonScrapper = require('./scrapper/amazonScrapper');
-const LazadaScrapper = require('./scrapper/lazadaScrapper');
-
+const AmazonScrapper = require("./scrapper/amazonScrapper");
+const LazadaScrapper = require("./scrapper/lazadaScrapper");
+const shopeeScrapper = require("./scrapper/shopeeScrapper");
+const qoo10Scrapper = require("./scrapper/qoo10Scrapper");
+const scrappingFilter = require("./scrapper/scrappingFilters");
 
 const app = express();
 
-require('dotenv').config();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 8888;
 
 // make sure port 5000 is working
-app.get('/', (req, res) => { 
-    res.send('Hello from node.js!');
-})
+app.get("/", (req, res) => {
+  res.send("Hello from node.js!");
+});
 // view engine set-up for express handlebars for html parsing
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 
 //BodyParser Middleware
-app.use(cors({
-    credentials: true, 
-    origin: ['http://localhost:3000'], // only localhost 3000 can have cookies !!! REMEMBER TO REPLACE with Hosted Service!!!
-}));
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:3000"], // only localhost 3000 can have cookies !!! REMEMBER TO REPLACE with Hosted Service!!!
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
 // Mongoose Middleware
 const uri = process.env.ATLAS_URI; // uses the .env file for password and link to MongdDB
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true,  useFindAndModify: false, },
-    (err) => {
-        if (err) return console.error(err)
-    });
+mongoose.connect(
+  uri,
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  },
+  (err) => {
+    if (err) return console.error(err);
+  }
+);
 
 const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log("MongoDB database connection established successfully!");
-})
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully!");
+});
 
 // these are my API end points in URLS
-app.use('/CPUs', CPURouter); 
-app.use('/GPUs', GPURouter); 
-app.use('/users', UserRouter);
-app.use('/Cards', CardRouter);
-app.use('/Builder',BuilderRouter)
-app.use('/RAMs', RAMRouter);
-app.use('/Mobos', MoboRouter);
-app.use('/PSUs', PSURouter)
-app.use('/Storage',StorageRouter);
+app.use("/CPUs", CPURouter);
+app.use("/GPUs", GPURouter);
+app.use("/users", UserRouter);
+app.use("/Cards", CardRouter);
+app.use("/Builder", BuilderRouter);
+app.use("/RAMs", RAMRouter);
+app.use("/Mobos", MoboRouter);
+app.use("/PSUs", PSURouter);
+app.use("/Storage", StorageRouter);
 // app.use('/Ascrapper',AmazonRouter);
 // app.use('/LazadaScrapper', LazadaRouter);
 
-app.get('/Ascrapper/:id', async (req, res) => {
-    const input = decodeURIComponent(req.params.id)
-    res.set({
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-        'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Content-Type': 'text/plain'
-    })
-    try {
-        console.log('Amazon Request Recieved')
-        const answer = await AmazonScrapper.Ascrapper(input, 10)
-        console.log('Sending your answer!')
-        res.json(answer)
-        
-    } catch(err) {
-        console.log('Never Give Up! Try Again!')
-        res
-        .status(500)
-        .json({Error : err})
-    }
-    
-})
+app.get("/Ascrapper/:id", async (req, res) => {
+  const input = decodeURIComponent(req.params.id);
+  res.set({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": true,
+    "Access-Control-Allow-Methods": "POST, GET, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Content-Type": "text/plain",
+  });
+  try {
+    console.log("Amazon Request Recieved");
+    const answer = await AmazonScrapper.Ascrapper(input, 10);
+    console.log("Sending your answer!");
+    res.json(answer);
+  } catch (err) {
+    console.log("Never Give Up! Try Again!");
+    res.status(500).json({ Error: err });
+  }
+});
 
-app.get('/LazadaScrapper/:id', async (req, res) => {
-    const input = decodeURIComponent(req.params.id)
-    res.set({
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-        'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Content-Type': 'text/plain'
-    })
+app.get("/LazadaScrapper/:id", async (req, res) => {
+  const input = decodeURIComponent(req.params.id);
+  res.set({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": true,
+    "Access-Control-Allow-Methods": "POST, GET, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Content-Type": "text/plain",
+  });
 
-    try {
-        console.log("Lazada Request Recieved")
-        const answer = await LazadaScrapper.LazScrapper(input, 10) // lazada scrapper
-        // answer = LazadaScrapper.lazExcludes(answer ,'itemName', 'Pre-Order') // excludes all items with pre order in name
-        console.log('sending answer')
-        res.json(answer)
-
-    } catch(err) {
-        res
-        .status(500)
-        .json({Error : err})
-    }
-})
+  try {
+    console.log("Lazada Request Recieved");
+    const answer = await LazadaScrapper.LazScrapper(input, 10); // lazada scrapper
+    // answer = LazadaScrapper.lazExcludes(answer ,'itemName', 'Pre-Order') // excludes all items with pre order in name
+    console.log("sending answer");
+    res.json(answer);
+  } catch (err) {
+    res.status(500).json({ Error: err });
+  }
+});
 
 // serve up static assets
-if (process.env.NODE_ENV === 'production') {
-    app.use('/static',express.static(path.join(__dirname, '../build'))); // hopefully this works
-    console.log('production mode activated')
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../build/index.html')); // hopefully this is the right directionary
-})
+if (process.env.NODE_ENV === "production") {
+  app.use("/static", express.static(path.join(__dirname, "../build"))); // hopefully this works
+  console.log("production mode activated");
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../build/index.html")); // hopefully this is the right directionary
+  });
 }
 
-app.listen(PORT, () => console.log(`Server Running at ${PORT}, Awesome!`))
+app.listen(PORT, () => console.log(`Server Running at ${PORT}, Awesome!`));
